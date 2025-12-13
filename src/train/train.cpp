5fd23381e07869b02e0a2757b23672cb1789909c
Abrams <yuanchianchen@gmail.com>
2025-12-13 15:28:17 +0800

    fix: train logic update

--- src/train/train.cpp
+++ src/train/train.cpp
@@ -4,17 +4,18 @@
 #include <stdlib.h>
 
 //config
-const int row = 3812;
-const int col = 32;
+const int trainSetNum = 2848;
+const int testSetNum = 713;
+const int col = 31;
 const int maxTrainTime = 1000000;
+const int printPerTime = 1;
 const float learnRate = 0.00001;
-const float testSetRatio = 0.2;
-const char dataPath[100] = "../../data/data467050_std.csv";
-const char weightPath[100] = "../../data/weight.csv";
 
-//value
-const int testSetNum = row * testSetRatio;
-const int trainSetNum = row - testSetNum;
+const char trainSetDataPath[100] = "../../data/trainSetDataFile467050_std.csv";
+const char trainSetRainDataPath[100] = "../../data/trainSetRainDataFile467050_std.csv";
+const char testSetDataPath[100] = "../../data/testSetDataFile467050_std.csv";
+const char testSetRainDataPath[100] = "../../data/testSetRainDataFile467050_std.csv";
+const char weightPath[100] = "../../data/weight.csv";
 
 //function
 double dot(double* a, float* b, int len){
@@ -27,76 +28,100 @@ double dot(double* a, float* b, int len){
 	return total;
 }
 
-double cost(double *w, float **x, int setNum){
+double cost(double *w, float **x, float *y, int setNum){
 	double totalCost = 0;
 
 	for (int i = 0; i < setNum - 1; i++) {
-		totalCost += (dot(w, x[i], col) - x[i + 1][col - 1]) * (dot(w, x[i], col) - x[i + 1][col - 1]);
+		totalCost += (dot(w, x[i], col) - y[i]) * (dot(w, x[i], col) - y[i]);
 	}
 
 	return totalCost;
 }
 
-double PartialDerivative(double *w, float *x, float *x2, int idx){
-	return 2 * (dot(w, x, col) - x2[col-1]) * x[idx];
+double PartialDerivative(double *w, float *x, float y, int idx){
+	return 2 * (dot(w, x, col) - y) * x[idx];
 }
 
 //run
 int main() {
 	//value
-	float** trainSet = (float **)malloc(trainSetNum * sizeof(float*));
+	float** trainSet = (float**)malloc(trainSetNum * sizeof(float*));
 	for (int i = 0; i < trainSetNum; i++) {
-		trainSet[i] = (float *)malloc((col) * sizeof(float));
+		trainSet[i] = (float*)malloc((col) * sizeof(float));
 	}
+
+	float* trainSetRain = (float *)malloc(trainSetNum * sizeof(float));
+
 	float** testSet = (float**)malloc(testSetNum * sizeof(float*));
 	for (int i = 0; i < testSetNum; i++) {
 		testSet[i] = (float*)malloc((col) * sizeof(float));
 	}
+
+	float* testSetRain = (float*)malloc(testSetNum * sizeof(float));
+
 	float id = 0;
 
 	//read file
 	FILE* dataFile;
+	FILE* rainDataFile;
 
-	if ((dataFile = fopen(dataPath, "r")) == NULL) {
+	if ((dataFile = fopen(trainSetDataPath, "r")) == NULL) {
+		printf("File could not be opened\n");
+		return 0;
+	}
+
+	if ((rainDataFile = fopen(trainSetRainDataPath, "r")) == NULL) {
 		printf("File could not be opened\n");
 		return 0;
 	}
 
 	//remove header
 	char header[2000] = "";
+	fscanf(rainDataFile, "%[^\n]\n", header);
 	fscanf(dataFile, "%[^\n]\n", header);
 
-	//printf("Reading and set trainSet...\n");
 	for (int i = 0; i < trainSetNum; i++) {
 		for (int j = 0; j < col; j++) {
 			if(j==col-1){
 				fscanf(dataFile, "%f\n", &trainSet[i][j]);
-				//printf("%.2f,", trainSet[i][j]);
 			}
 			else {
 				fscanf(dataFile, "%f,", &trainSet[i][j]);
-				//printf("%.2f,", trainSet[i][j]);
 			}
 		}
-		//printf("\n");
+
+		fscanf(rainDataFile, "%f\n", &trainSetRain[i]);
 	}
 
-	//printf("Reading and set testSet...\n");
+	if ((dataFile = fopen(testSetDataPath, "r")) == NULL) {
+		printf("File could not be opened\n");
+		return 0;
+	}
+
+	if ((rainDataFile = fopen(testSetRainDataPath, "r")) == NULL) {
+		printf("File could not be opened\n");
+		return 0;
+	}
+
+	//remove header
+	fscanf(rainDataFile, "%[^\n]\n", header);
+	fscanf(dataFile, "%[^\n]\n", header);
+
 	for (int i = 0; i < testSetNum; i++) {
 		for (int j = 0; j < col; j++) {
 			if (j == col - 1) {
 				fscanf(dataFile, "%f\n", &testSet[i][j]);
-				//printf("%.2f,", testSet[i][j]);
 			}
 			else {
 				fscanf(dataFile, "%f,", &testSet[i][j]);
-				//printf("%.2f,", testSet[i][j]);
 			}
 		}
-		//printf("\n");
+
+		fscanf(rainDataFile, "%f\n", &testSetRain[i]);
 	}
 
 	fclose(dataFile);
+	fclose(rainDataFile);
 
 	//printf("trainSet:\n");
 	//for (int i = 0; i < trainSetNum; i++) {
@@ -128,30 +153,27 @@ int main() {
 		bestWeight[i] = 1;
 	}
 
-	while (bestCost > 1000 && count < maxTrainTime) {
-		for (int i = 0; i < trainSetNum - 1 && count < maxTrainTime; i++) {
+	while (bestCost > 100 && count < maxTrainTime) {
+		for (int i = 0; i < trainSetNum && count < maxTrainTime; i++) {
 			count++;
 
 			//record
-			currentCost = cost(weight, testSet, testSetNum);
-			printf("ID:%5d, cost:%10f\n", count, currentCost);
+			currentCost = cost(weight, testSet, testSetRain, testSetNum);
+			if (count % printPerTime == 0) {
+				printf("ID:%5d, cost:%10f\n", count, currentCost);
+			}
 			if (count == 1) {
 				bestCost = currentCost;
 				for (int j = 0; j < col; j++) {
 					bestWeight[j] = weight[j];
 				}
-				//printf("ID:%5d, cost:%10f\n", count, currentCost);
 			}
 			else {
 				if (currentCost < bestCost) {
 					printf("----------\nBest Weight Updated\n");
-					//printf("ID:%5d, cost:%10f\n", count, currentCost);
 					bestCost = currentCost;
 					for (int j = 0; j < col; j++) {
 						bestWeight[j] = weight[j];
-					}
-					for (int k = 0; k < col; k++) {
-						//printf("%f,",weight[k]);
 					}
 					printf("\n----------\n");
 				}
@@ -159,8 +181,7 @@ int main() {
 
 
 			for (int j = 0; j < col; j++) {
-				weight[j] -= learnRate * PartialDerivative(weight, trainSet[i],trainSet[i+1], j);
-				//printf("Index:%5d, gradient:%10f\n", j, gradient(weight, trainSet[i], trainSet[i + 1], j));
+				weight[j] -= learnRate * PartialDerivative(weight, trainSet[i], trainSetRain[i], j);
 			}
 		}
 	}
